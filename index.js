6e4464a945d76b2ef9782f57531ab56b5c44f206
seungwan Ham <seungwan7511@gmail.com>
2022-06-04 13:54:04 +0900

    ADD HTML : error404,500

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 
 const PORT = 5100;
 
-app.get("/:epoch_seconds", function (req, res) {
+app.get("/:epoch_seconds", function (req, res, body) {
   const { epoch_seconds } = req.params;
   const epoch_to_string = convert.epoch_to_ISOString(epoch_seconds);
   const data = {
@@ -14,6 +14,16 @@ app.get("/:epoch_seconds", function (req, res) {
     data: epoch_to_string,
   };
   res.json(data);
+});
+
+app.use(express.static("public"));
+
+app.use(function (req, res, next) {
+  res.status(404).sendFile(__dirname + "/views/error404.html");
+});
+
+app.use(function (err, req, res, next) {
+  res.status(500).sendFile(__dirname + "/views/error500.html");
 });
 
 // PORT setting
