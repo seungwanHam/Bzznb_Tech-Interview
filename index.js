378c529d89cda6377a5f69d0c4fb38fdbd2b297c
seungwan Ham <seungwan7511@gmail.com>
2022-06-04 21:09:30 +0900

    modify statusMessage

--- index.js
+++ index.js
@@ -8,19 +8,21 @@ const PORT = 5100;
 app.get("/:epoch_seconds", function (req, res) {
   const { epoch_seconds } = req.params;
   const epoch_to_string = convert.epoch_to_ISOString(epoch_seconds);
-  // res.writeHead(200, "Current password does not match");
-  const msg = res.stausCode == 200 || 304 ? "OK" : "nop";
-  const data = {
+  const msg =
+    res.statusCode == 200 || 304
+      ? (res.statusMessage = "OK")
+      : (res.statusMessage = "Error");
+  const result = {
     status: res.statusCode,
     message: msg,
     data: epoch_to_string,
   };
-  res.send(data);
+  res.send(result);
 });
 
 app.use(express.static("public"));
 
-app.use(function (req, res, next) {
+app.use(function (req, res) {
   res.status(404).sendFile(__dirname + "/views/error404.html");
 });
 
@@ -28,5 +30,4 @@ app.use(function (err, req, res, next) {
   res.status(500).sendFile(__dirname + "/views/error500.html");
 });
 
-// PORT setting
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
